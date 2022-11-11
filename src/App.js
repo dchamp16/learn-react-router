bbf2b908a96d0dd9d0db9f4e9d232046a1be0d0e
dchamp16 <dchamp16@gmail.com>
2022-11-10 22:09:59 -0700

    added some routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import BookList from "./pages/BookList";
 
 function App() {
-  return <h1>Testing React</h1>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function BookList() {
-  return <div>BookList</div>;
+  return <h1>Books</h1>;
 }
